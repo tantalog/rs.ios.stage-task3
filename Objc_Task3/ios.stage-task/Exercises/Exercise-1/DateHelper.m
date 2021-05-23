a70ddf99066870442af8cf305f86ae20e34aba2a
tantalog <kushnerovanton@gmail.com>
2021-05-24 00:54:32 +0300

    feat: Exercise 1, Exercise 2 complete

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,24 +5,51 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    if ( (monthNumber < 1) || (monthNumber > 13)) {
+        return nil;
+    }
+
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSString *monthString = dateFormatter.monthSymbols[(monthNumber-1)];
+        
+    return monthString;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+   
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
+     NSDate * tempDate = [formatter dateFromString:date];
+    
+    [formatter setDateFormat:@"dd"];
+
+    long dayOfMonth = [[ formatter stringFromDate:tempDate] longLongValue];
+    return dayOfMonth;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    [dateFormatter setLocalizedDateFormatFromTemplate:@"E"];
+    NSString *strDate = [dateFormatter stringFromDate:date];
+    return strDate;
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDate *dateNow = [NSDate date];
+
+    if((long)[[calendar components: NSCalendarUnitWeekOfYear fromDate:date] weekOfYear] == (long)[[calendar components: NSCalendarUnitWeekOfYear fromDate:dateNow] weekOfYear]){
+        return YES;
+    }
+    
     return NO;
 }
 
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,36 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    if (ratingArray.count <= 2 || ratingArray == nil) {
+        return  0;
+    }
+    
+    NSInteger n = ratingArray.count;
+    NSInteger counter = 0;
+    
+    for (NSInteger i = 0; i < n - 2 ; i++) {
+        for (NSInteger j = 1; j < n - 1 ; j++) {
+            for (NSInteger k = 2; k < n ; k++) {
+                
+                
+                if (([ratingArray[i] intValue] < [ratingArray[j] intValue ]) && ([ratingArray[j] intValue ] < [ratingArray[k] intValue ]))   {
+                    
+                    if ((0 <= i) && (i < j) && (j < k) && (k < n)) {
+                        counter += 1;
+                    }
+                    
+                } else if (([ratingArray[i] intValue] > [ratingArray[j] intValue ]) && ([ratingArray[j] intValue ] > [ratingArray[k] intValue ])) {
+                    
+                    if ((0 <= i) && (i < j) && (j < k) && (k < n)){
+                        counter += 1;
+                    }
+                }
+            }
+        }
+    }
+    
+    return counter;
 }
 
 @end
