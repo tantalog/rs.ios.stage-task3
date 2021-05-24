3fd2d0571a7228ac52b755a2e6809beea7bb4a13
tantalog <kushnerovanton@gmail.com>
2021-05-24 20:46:00 +0300

    feat: Exercise 3 complete

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,29 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSMutableString *firstString = [NSMutableString stringWithString:string1];
+    NSMutableString *secondString = [NSMutableString stringWithString:string2];
+    NSMutableString *result = [[NSMutableString alloc] init];
+    
+    while ((firstString.length != 0) && (secondString.length != 0)) {
+        if ([firstString UTF8String] [0] <= [secondString UTF8String] [0]) {
+            [result appendString: [firstString substringToIndex: 1]];
+            firstString = [firstString substringFromIndex: 1];
+        }
+        else {
+            [result appendString: [secondString substringToIndex: 1]];
+            secondString = [secondString substringFromIndex: 1];
+        }
+    }
+    
+    if (firstString.length == 0) {
+        [result appendString: secondString];
+    }
+    else {
+        [result appendString: firstString];
+    }
+    return result;
 }
 
 @end
