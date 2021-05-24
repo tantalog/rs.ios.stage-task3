12b44bd806f42780d678974a413d665a3f21d8b4
tantalog <kushnerovanton@gmail.com>
2021-05-24 22:00:54 +0300

    feat: Exercise 4 complete

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,30 @@
 #import "LevelOrderTraversal.h"
 
-NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    return nil;
+NSUInteger Integer(NSArray *treeArray, NSMutableArray *levelOrdered, NSUInteger level, NSUInteger startIndex) {
+    typedef enum {
+        Left,
+        Right
+    }
+    Order;
+    
+    NSUInteger index = startIndex;
+    for (Order nodeType = Left; nodeType <= Right && index < treeArray.count; ++nodeType) {
+        NSObject *object = treeArray[index++];
+        if ([object isKindOfClass:NSNumber.class]) {
+            if (levelOrdered.count <= level) {
+                [levelOrdered addObject:[NSMutableArray new]];
+            }
+            [levelOrdered[level] addObject:object];
+            index = Integer(treeArray, levelOrdered, level+1, index);
+        }
+    }
+    return index;
+}
+
+NSArray *LevelOrderTraversalForTree(NSArray *tree)
+{
+    NSMutableArray *resultArray = [[NSMutableArray alloc] init];
+        NSUInteger level = 0;
+        Integer(tree, resultArray, level, 0);
+        return [resultArray copy];
 }
